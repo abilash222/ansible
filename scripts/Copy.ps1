41687b3ba0316e81e8603f189abeedb69d2eaa46
abilash222 <abilash_222@yahoo.co.in>
2016-10-24 21:23:41 +0530

    update

--- scripts/Copy.ps1
+++ scripts/Copy.ps1
@@ -1,3 +1,4 @@
 #New-Item -ItemType directory -Path C:\workspace
 #Copy-Item \\CTSC00280492601\COESharedFolder\592581\workspace\** C:\workspace
-xcopy /E \\CTSC00280492601\COESharedFolder\592581\workspace C:\workspace
+net use z: \\CTSC00280492601\COESharedFolder\
+xcopy /E z:\592581\workspace C:\workspace
