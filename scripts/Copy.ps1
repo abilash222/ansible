e4cb745d64c7e8547f767a663fb21a2ed6c78525
abilash222 <abilash_222@yahoo.co.in>
2016-10-24 21:43:57 +0530

    update

--- scripts/Copy.ps1
+++ scripts/Copy.ps1
@@ -1,4 +1,6 @@
 #New-Item -ItemType directory -Path C:\workspace
 #Copy-Item \\CTSC00280492601\COESharedFolder\592581\workspace\** C:\workspace
-net use z: \\CTSC00280492601\COESharedFolder\
-xcopy /E z:\592581\workspace C:\workspace
+$cred = Get-Credential
+$networkCred = $cred.GetNetworkCredential()
+net use \\CTSC00280492601\COESharedFolder\592581\ $networkCred.Password /USER:$networkCred.UserName
+Copy-Item \\CTSC00280492601\COESharedFolder\592581\workspace\** C:\workspace
